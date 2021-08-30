93498ac72c2e3c356818848b934758be1c4e646d
anwarmontasir <anwarmontasir@gmail.com>
2021-08-30 13:27:45 -0700

    add error page

--- app.js
+++ app.js
@@ -9,6 +9,18 @@ app.set('view engine', 'pug');
 const mainRoutes = require('./routes');
 app.use(mainRoutes);
 
+app.use((req, res, next) => {
+    const err = new Error('That page is not found.');
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('error');
+})
+
 app.listen(3000, () => {
     console.log('the application is running on localhost:3000');
 })
